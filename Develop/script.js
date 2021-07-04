b850e51fc347443ab50dbe45b600e53f871d667e
tobikehinde22 <85264332+tobikehinde22@users.noreply.github.com>
2021-07-04 12:09:21 -0500

    Added questions for password Specifics

--- Develop/script.js
+++ Develop/script.js
@@ -5,9 +5,27 @@ function generatePassword() {
 
 // make array of all characters to be used
 var upperArray = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+
 var lowerArray = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+
 var numberArray = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+
 var specialArray = ["@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "=", "+"];
+
+//array place holders
+var responseArray = [];
+
+var clientArray = [];
+
+// Password generator specificty
+
+var amountCharacters = prompt("How many numbers between 8 and 128 do you want in your passowrd?");
+var numberAmount = confirm("Would you like numbers included in your password?");
+var upperCase = confirm("Would you like uppercase characters in your password?");
+var lowerCase = confirm("Would you like lower case characters in your password?");
+var specialCharacters = confirm("Would you like Special Characters in your password?");
+
+
 
 
 // Get references to the #generate element
